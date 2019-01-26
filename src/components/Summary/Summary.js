ace4b4c108c966004e3774b25fb0d73b51148e15
Maxwell Todd <mtmaxtodd@gmail.com>
2019-01-25 20:27:07 -0600

    Added a button to the summary component that shows on every page. Conditionally rendered the button to say incomplete and to be disabled until every form is filled.

--- src/components/Summary/Summary.js
+++ src/components/Summary/Summary.js
@@ -4,23 +4,27 @@ import { connect } from 'react-redux';
 class Summary extends Component {
 
     render() {
+        let status = '';
         let buttonText = '';
         if (this.props.reduxStore.feelings==''||
             this.props.reduxStore.understanding == '' ||
             this.props.reduxStore.support == '' ||
             this.props.reduxStore.comments == ''){
                 buttonText = "Incomplete"
+            status = true;
+                
         }else{
             buttonText = "Submit"
+            status = false;
         }
         return (
-            <div>
+            <form>
                 <p>Feelings: {this.props.reduxStore.feelings}</p>
                 <p>Understanding: {this.props.reduxStore.understanding}</p>
                 <p>Support: {this.props.reduxStore.support}</p>
                 <p>Comments: {this.props.reduxStore.comments}</p>
-                <button onClick>{buttonText}</button>
-            </div>
+                <button id ="submit"type="submit" disabled={status}>{buttonText}</button>
+            </form>
         )
     }
 }
