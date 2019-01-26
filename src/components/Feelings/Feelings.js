972121aaff1bad33a3dc168e10d04e62ba77d25b
Maxwell Todd <mtmaxtodd@gmail.com>
2019-01-26 12:31:29 -0600

    Added material ui to project. I made a card for the feelings page content.

--- src/components/Feelings/Feelings.js
+++ src/components/Feelings/Feelings.js
@@ -1,17 +1,24 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import './Feelings.css';
+import Card from '@material-ui/core/Card';
+import MenuItem from '@material-ui/core/MenuItem';
+import TextField from '@material-ui/core/TextField';
+import Input from '@material-ui/core/Input';
+import Button from '@material-ui/core/Button';
+import CardContent from '@material-ui/core/CardContent';
 
 class Feelings extends Component {
-    constructor(){
+    constructor() {
         super();
-        this.state={
-            input:'',
+        this.state = {
+            input: '',
         }
     }
 
     handleNextButton = (event) => {
         console.log('input', this.state.input);
-        
+
         const action = { type: 'SET_FEELINGS', payload: this.state.input };
         this.props.dispatch(action);
         this.setState({
@@ -22,7 +29,7 @@ class Feelings extends Component {
 
     updateFeelings = (event) => {
         console.log('update feelings');
-        
+
         this.setState({
             input: event.target.value,
         })
@@ -30,14 +37,17 @@ class Feelings extends Component {
 
     render() {
         return (
-            <div>
-            <h1>How are you feeling today?</h1>
-            <input type="number" placeholder="1-5, 5 is best"
-                onChange={this.updateFeelings}/>
-            <button onClick={this.handleNextButton}>Next</button>
-            
+            <div className="container">
+                <Card raised="true" className="card">
+                    <h1>How are you feeling today?</h1>
+                    <CardContent className="content">
+                        <TextField variant="outlined"type="number" placeholder="1-5, 5 is best"
+                            onChange={this.updateFeelings} />
+                        <Button size="large"variant="contained" onClick={this.handleNextButton}>Next</Button>
+                    </CardContent>
+                </Card>
             </div>
-            )
+        )
     }
 }
 
