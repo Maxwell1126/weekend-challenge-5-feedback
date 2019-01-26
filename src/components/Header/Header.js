e3a0dad73766a367566d06b649979408ded8a93f
Maxwell Todd <mtmaxtodd@gmail.com>
2019-01-26 11:25:25 -0600

    Added styling to the header component with the new file header.css.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import './Header.css';
 
 class Header extends Component {
     render() {
         return (
-            <header >
-                <h1 className="App-title">Feedback!</h1>
-                <h4><i>Don't forget it!</i></h4>
+            <header className="head">
+                <h1 >Feedback</h1>
             </header>
         )
     }
